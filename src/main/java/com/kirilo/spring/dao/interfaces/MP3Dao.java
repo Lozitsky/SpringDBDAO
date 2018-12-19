29fe38741093eb45ecfc66db8c24869f270267e3
Kirilo Lozitsky <bondxxl01@gmail.com>
2018-12-19 17:35:03 +0200

    created MP3DAO interface, MP3 model and MyBeanPostProcessor class, added all dependencies, Spring context.

---
+++ src/main/java/com/kirilo/spring/dao/interfaces/MP3Dao.java
@@ -0,0 +1,13 @@
+package com.kirilo.spring.dao.interfaces;
+
+import com.kirilo.spring.dao.objects.MP3;
+
+import java.util.List;
+
+public interface MP3Dao {
+    void insert (MP3 mp3);
+    void  delete (MP3 mp3);
+    MP3 getMP3ById(int id);
+    List<MP3> getMP3ListByName(String name);
+    List<MP3> getMP3ListByAuthor(String name);
+}
